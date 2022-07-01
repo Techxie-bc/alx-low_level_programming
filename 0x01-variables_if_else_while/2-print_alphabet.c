afa472291171e01c3b547f8474df67cda9701951
Techxie-bc <donnaogbu@gmail.com>
2022-07-01 06:29:14 -0700

    happy

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - prints function that
+* dislays lowercase alphabets
+*
+* Return: Always 0
+*/
+int main(void)
+{
+char write;
+for (write = 'a'; write <= 'z'; write++)
+putchar(write);
+putchar('\n);
+return (0);
+}
