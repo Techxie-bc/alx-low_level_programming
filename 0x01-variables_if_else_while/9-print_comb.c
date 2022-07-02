9120dad4de030c973a6f631c6a356f6cc94bed95
Techxie-bc <donnaogbu@gmail.com>
2022-07-01 18:42:47 -0700

    9 final

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,11 +13,12 @@ int main(void)
 int num;
 for (num = 0; num <= 9; num++)
 {
+if (num != 9)
 putchar((num % 10) + '0');
-if (num == '9')
-continue;
 putchar(',');
 putchar(' ');
+else
+putchar((num % 10) + '0');
 }
 putchar('\n');
 return (0);
