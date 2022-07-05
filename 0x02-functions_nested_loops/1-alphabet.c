604d444da228ddca72041018e84d0cff9b8cda2b
Techxie-bc <donnaogbu@gmail.com>
2022-07-05 05:19:25 -0700

    for lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_alphabet - Print alphabets in lowercase
+*
+* Return: Always (0)
+*/
+void print_alphabet(void)
+{
+int n;
+for (n = 'a'; n <= 'z'; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
+}
