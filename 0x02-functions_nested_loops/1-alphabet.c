b29cc4446a65365595acc49a913e81ada5b74487
Techxie-bc <donnaogbu@gmail.com>
2022-07-05 05:28:24 -0700

    last

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,14 @@
 
 /**
 * print_alphabet - Prints alphabets in lowercase
-*
-* Return: Always (0)
+* followed by a new line
 */
 void print_alphabet(void)
 {
 char n;
 for (n = 'a'; n <= 'z'; n++)
-{
+
 _putchar(n);
-}
+
 _putchar('\n');
 }
