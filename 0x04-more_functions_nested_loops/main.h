2e311f6c01b90b31638da49749527250d8f73908
Techxie-bc <donnaogbu@gmail.com>
2022-07-07 05:34:39 -0700

    main

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,49 +1,33 @@
-ifndef HOLBERTON_H
+#ifndef MAIN_H
 
-#define HOLBERTON_H
+#define MAIN_H
 
 
-
-int _putchar(char c);
-
-int _islower(int c);
-
-int _isalpha(int c);
-
-int _abs(int n);
 
 int _isupper(int c);
 
 int _isdigit(int c);
 
-int _strlen(char *s);
+int mul(int a, int b);
 
-void _puts(char *s);
+void print_numbers(void);
 
-char *_strcpy(char *dest, char *src);
+int _putchar(char c);
 
-int _atoi(void);
+void print_most_numbers(void);
 
-char *_strcat(char *dest, char *src);
+void more_numbers(void);
 
-char *_strncat(char *dest, char *src, int n);
+void print_line(int n);
 
-char *_strncpy(char *dest, char *src, int n);
+void print_diagonal(int n);
 
-int _strcmp(char *s1, char *s2);
+void print_square(int size);
 
-char *_memset(char *s, char b, unsigned int n);
+void print_triangle(int size);
 
-char *_memcpy(char *dest, char *src, unsigned int n);
-
-char *_strchr(char *s, char c);
-
-unsigned int _strspn(char *s, char *accept);
-
-char *_strpbrk(char *s, char *accept);
-
-char *_strstr(char *haystack, char *needle);
+void print_number(int n);
 
 
 
-#endif /* HOLBERTON_H */
+#endif
