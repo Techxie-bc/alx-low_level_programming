d97025099dff84594fe6dd9f0a54d33ac9174885
Techxie-bc <donnaogbu@gmail.com>
2022-07-07 09:00:01 -0700

    100

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_triangle - print
+* a triangle
+* @size: differing integer
+* Return: Always 0
+*/
+void print_triangle(int size)
+{
+int x,y;
+if (y <= size - x)
+{
+_putchar(' ');
+}
+else
+{
+_putchar('#');
+}
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
