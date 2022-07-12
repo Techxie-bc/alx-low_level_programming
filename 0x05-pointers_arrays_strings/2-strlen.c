b6732e38727ec8065dcc178a942b647b40e36e6d
Techxie-bc <donnaogbu@gmail.com>
2022-07-12 05:03:09 -0700

    num 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _strlen - returns the length
+* of a string
+* @s: character to be checked
+* Return: Return length of the string
+*/
+int _strlen(char *s)
+{
+int len;
+for (len = 0; *s != '\0'; len++)
+{
+s++
+}
+return (len);
+}
