3d1e4baf0184c43171fc8b03cbc333edc3f53074
Techxie-bc <donnaogbu@gmail.com>
2022-07-20 02:47:24 -0700

    ok

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,7 +11,7 @@ int find_strlen(char *a, int l)
 {
 if (*a == 0)
 return (l - 1);
-return (palind2(a + 1, l + 1));
+return (find_strlen(a + 1, l + 1));
 }
 
 /**
@@ -27,7 +27,7 @@ if (*a != *(a + l))
 return (0);
 else if (*a == 0)
 return (1);
-return (palind3(a + 1, l - 2));
+return (check_palindrome(a + 1, l - 2));
 }
 
 /**
