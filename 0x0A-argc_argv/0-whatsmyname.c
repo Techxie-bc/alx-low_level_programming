81b06f6ea778edf58e1588019dfa094a5f2d7ed3
Techxie-bc <donnaogbu@gmail.com>
2022-07-22 20:07:55 -0700

    no 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - prints its name
+* @argc: argument count
+* @argv: pointers to array of string
+* Return: Always (0)
+*/
+int main(int argc, char *argv[])
+{
+printf("%s\m", *argv);
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+this readme is for argc, argv project in c
