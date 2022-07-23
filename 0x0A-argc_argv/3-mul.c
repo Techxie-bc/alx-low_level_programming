bfdc61a78a0e53937aa4f1a7a72813f981656ce2
Techxie-bc <donnaogbu@gmail.com>
2022-07-22 20:41:46 -0700

    no 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - multiplies to numbers
+* @argc: argument count
+* @argv: array of string
+* Return: Always (0)
+*/
+int main(int argc, char *argv[])
+{
+int a, b, result;
+if (argc <= 2)
+{
+printf("Error\n");
+return (1);
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+result = (a * b);
+printf("%d\n", result);
+return (0);
