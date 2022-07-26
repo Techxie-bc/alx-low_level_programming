9683303c2f1e818b6f99f043559a376d3a434477
Techxie-bc <donnaogbu@gmail.com>
2022-07-26 05:42:48 -0700

    n0 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,23 +1,24 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-* create_array - creates an array of chars
-* @size: the size of the array
-* to be initiated
-* @c: the specific char to initiate
-* the array with
+* _strdup - creates a pointer to a newly
+* allocated space in memory
+* @str: the string to be copied
 * Return: 0 or NULL
 */
-char *create_array(unsigned int size, char c)
+char *_strdup(char *str)
 {
-char *arr;
-unsigned int index;
-if (size == 0)
+char *copy;
+int index, len = 0;
+if (str == NULL)
 return (NULL);
-arr = malloc(sizeof(char) * size);
-if (arr == NULL)
+for (index = 0; str[index]; index++)
+len++;
+copy = malloc(sizeof(char) * (len + 1));
+if (copy == NULL)
 return (NULL);
-for (index = 0; index < size; index++)
-arr[index] = c;
-return (arr);
+for (index = 0; str[index]; index++)
+copy[index] = str[index];
+copy[len] = '\0';
+return (duplicate);
 }
