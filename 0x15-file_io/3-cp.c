ea1709be4be6f49d66ba0e1daa66d25c3ac6fedc
Techxie-bc <donnaogbu@gmail.com>
2022-08-22 19:57:16 -0700

    fixed

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -90,7 +90,7 @@ free(buffer);
 exit(99);
 }
 r = read(from, buffer, 1024);
-to = open(argv[2], O_WRONLY | O_APPEND)
+to = open(argv[2], O_WRONLY | O_APPEND);
 } while (r > 0);
 free(buffer);
 close_file(from);
